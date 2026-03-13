d24c9e52405ab68eaf1d08722c4e1a48be01278a
oodonnel <oodonnel@uwo.ca>
2026-03-13 12:17:48 -0400

    owen did something right

---
+++ Servo_TestCode/Servo_TestCode.ino
@@ -0,0 +1,28 @@
+#include <ESP32Servo.h>
+
+Servo testServo;
+
+// Change this to your servo signal pin
+const int SERVO_PIN = 17;
+
+void setup() {
+  Serial.begin(115200);
+
+  testServo.attach(SERVO_PIN);
+
+  Serial.println("Servo test starting...");
+}
+
+void loop() {
+  Serial.println("Moving to 0 degrees");
+  testServo.write(0);
+  delay(2000);
+
+  Serial.println("Moving to 90 degrees");
+  testServo.write(90);
+  delay(2000);
+
+  Serial.println("Moving to 180 degrees");
+  testServo.write(180);
+  delay(2000);
+}
